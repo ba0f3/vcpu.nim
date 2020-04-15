bcc58e7eae813923370571ca9773b9fb7a12305f
Huy Doan <me@huy.im>
2020-04-15 17:28:07 +0700

    remove with depends

--- src/vcpu.nim
+++ src/vcpu.nim
@@ -1,4 +1,4 @@
-import with, streams, macros, strutils
+import streams, macros, strutils
 
 const
   DATA_SIZE = 64
@@ -91,8 +91,7 @@ proc initVCPU*(): VCPU =
   result.reset()
 
 proc loadCode*(cpu: var VCPU, code: pointer, codeLen: int): bool =
-  when not defined(release):
-    echo "load code len: ", codeLen
+  dump "loading code, len:", codeLen
   copyMem(addr cpu.code, code, codeLen)
   cpu.codeLen = codeLen.DWORD
   return true
@@ -105,8 +104,7 @@ proc loadCode*(cpu: var VCPU, stream: Stream): bool {.inline.} =
   cpu.loadCode(code.cstring, code.len)
 
 proc addCode*(cpu: var VCPU, code: pointer, codeLen: DWORD): DWORD =
-  when not defined(release):
-    echo "add code len: ", codeLen
+  dump "adding code, len:", codeLen
   if cpu.codeLen + codeLen >= DATA_SIZE.DWORD:
     raise newException(BufferOverflowException, "buffer overflow")
   result = cpu.codeLen
@@ -136,333 +134,328 @@ proc jmp*(cpu: var VCPU, `addr`: DWORD): bool =
 
 proc run*(cpu: var VCPU) =
   var
-    opcode: OpCode
+    op: OpCode
     b0, b1, b2: BYTE
     w0, w1: WORD
     d0, d1: DWORD
 
-  with cpu:
-    while true:
-      if cpu.regs.PC >= codeLen: exception
-      cpu.read(opcode)
-      #when defined(x):
-      #  echo "OPCODE: ", opcode
-      case opcode
-      of NOP:
-        dump opcode
-        #opcode = OpCode(opcode.int + 20)
-        discard
-      of HALT:
-        dump opcode
-        break
-      of MOV:
-        cpu.read(b0)
-        cpu.read(b1)
-        dump opcode, b0.Regs, b1.Regs
-        if (b0 >= 0 and b0 <= 7) and (b1 >= 0 and b1 <= 7):
-          regs.R[b0] = regs.R[b1]
-        else:
-          error "invalid register"
-      of MOVMB:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(w0)
-        if w0 >= codeLen: exception
-        dump opcode, b0.Regs, w0
-        cpu.read(w1, w0)
-        regs.R[b0] = w1
-      of MOVMW:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(w0)
-        if w0 >= codeLen: exception
-        dump opcode, b0.Regs, w0
-        cpu.read(w1, w0)
-        regs.R[b0] = w1
-      of MOVB:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        dump opcode, b0.Regs, b1
-        regs.R[b0] = b1
-      of MOVW:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(w0)
-        dump MOVW, b0.Regs, w0
-        regs.R[b0] = w0
-      of MOVBM:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(w0)
-        dump opcode, b0.Regs, w0
-        if w0 >= codeLen:
-          exception
-        cpu.write(byte(regs.R[b0]), w0)
-      of MOVWM:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(w0)
-        dump opcode, b0.Regs, w0
-        if w0 >= codeLen: exception
-        cpu.write(regs.R[b0], w0)
-      of MOVMRB:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        dump opcode, b0.Regs, b1.Regs
-        if b1 > 8: exception
-        if regs.R[b1] >= codeLen: exception
-        cpu.read(b2, regs.R[b1])
-        regs.R[b0] = b2
-      of MOVMRW:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        dump opcode, b0.Regs, b1.Regs
-        if b1 > 8: exception
-        if regs.R[b1] >= codeLen: exception
-        cpu.read(w0, regs.R[b1])
-        regs.R[b0] = w0
-      of MOVMD:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(d0)
-        dump opcode, b0.Regs, d0
-        if d0 >= codeLen: exception
-        regs.R[b0] = d0
-      of MOVD:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(d0)
-        dump opcode, b0.Regs, d0
-        if w0 >= codeLen: exception
-        regs.R[b0] = d0
-      of MOVDM:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(w0)
-        dump opcode, b0.Regs, w0
-        if w0 >= codeLen: exception
-        cpu.write(regs.R[b0], w0)
-      of MOVMRD:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        if regs.R[b1] >= codeLen: exception
-        cpu.read(d0, regs.R[b1])
-        regs.R[b0] = d0
-      of JMP:
-        cpu.read(w0)
-        dump opcode, w0
-        if w0 > codeLen: exception
-        regs.PC = w0
-      of JZ:
-        cpu.read(w0)
-        dump opcode, w0
-        if w0 > codeLen: exception
-        if regs.ZF == 1:
-          regs.PC = w0
-      of JNZ:
-        cpu.read(w0)
-        dump opcode, w0
-        if w0 > codeLen: exception
-        if regs.ZF == 1:
-          regs.PC = w0
-      of JAE:
-        cpu.read(w0)
-        dump opcode, w0
-        if w0 > codeLen: exception
-        if regs.ZF == 1 or regs.CF == 0:
-          regs.PC = w0
-      of JBE:
-        cpu.read(w0)
-        dump opcode, w0
-        if w0 > codeLen: exception
-        if regs.ZF == 1 or regs.CF == 1:
-          regs.PC = w0
-      of JB:
-        cpu.read(w0)
-        dump opcode, w0
-        if w0 > codeLen: exception
-        if regs.ZF == 0 and regs.CF == 1:
-          regs.PC = w0
-      of JA:
-        cpu.read(w0)
-        dump opcode, w0
-        if w0 > codeLen: exception
-        if regs.ZF == 0 and regs.CF == 0:
-          regs.PC = w0
-      of ADVR:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(w0)
-        dump opcode, b0.Regs, w0
-        w1 = regs.R[b0].WORD + w0
-        regs.ZF = if w1 == 0: 1 else: 0
-        regs.CF = if w1 < regs.R[b0]: 1 else: 0
-        regs.R[b0] = w1
-      of ADRR:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        d0 = regs.R[b0]
-        d1 = regs.R[b1]
-        d1 += d0
-        regs.R[b0] = d1
-        regs.ZF = if d1 == 0: 1 else: 0
-        regs.CF = if d1 < d0: 1 else: 0
-      of ADRRL:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        b2 = regs.R[b0].BYTE
-        b1 = regs.R[b1].BYTE
-        b1 = b1 + b2
-        regs.R[b0] = b1
-        regs.ZF = if b1 == 0: 1 else: 0
-        regs.CF = if b1 < b2: 1 else: 0
-      of SUBVR:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(w0)
-        dump opcode, b0.Regs, w0
-        w1 = regs.R[b0].WORD - w0
-        regs.ZF = if w1 == 0: 1 else: 0
-        regs.CF = if w1 > regs.R[b0]: 1 else: 0
-        regs.R[b0] = w1
-      of SUBRR:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        d0 = regs.R[b0]
-        d1 = regs.R[b1]
-        d1 -= d0
-        regs.R[b0] = d1
-        regs.ZF = if d1 == 0: 1 else: 0
-        regs.CF = if d1 > d0: 1 else: 0
-      of SUBRRL:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        b2 = regs.R[b0].BYTE
-        b1 = regs.R[b1].BYTE
-        b1 = b1 - b2
-        regs.R[b0] = b1
-        regs.ZF = if b1 == 0: 1 else: 0
-        regs.CF = if d1 > b2: 1 else: 0
-      of XOR:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        d0 = regs.R[b0]
-        d1 = regs.R[b1]
-        d0 = d0 xor d1
-        regs.ZF = if d0 == 0: 1 else: 0
-        regs.CF = 0
-        regs.R[b0] = d0
-      of XORL:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        b2 = regs.R[b0].BYTE
-        b1 = regs.R[b1].BYTE
-        b2 = b2 xor b1
-        regs.ZF = if b2 == 0: 1 else: 0
-        regs.CF = 0
-        regs.R[b0] = b2
-      of NOT:
-        cpu.read(b0)
-        if b0 > 8: exception
-        dump opcode, b0.Regs
-        regs.R[b0] = not regs.R[b0]
-      of NOTL:
-        cpu.read(b0)
-        if b0 > 8: exception
-        #cast[ptr BYTE](addr regs.R[b0])[] = not regs.R[b0]
-      of ADVRD:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(d0)
-        dump opcode, b0.Regs, d0
-        d1 = regs.R[b0] + d0
-        regs.ZF = if d1 == 0: 1 else: 0
-        regs.CF = if d1 < regs.R[b0]: 1 else: 0
-        regs.R[b0] = d1
-      of SUBVRD:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(d0)
-        dump opcode, b0.Regs, d0
-        d1 = regs.R[b0] - d0
-        regs.ZF = if d1 == 0: 1 else: 0
-        regs.CF = if d1 > regs.R[b0]: 1 else: 0
-        regs.R[b0] = d1
-      of CMP:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        d0 = regs.R[b0]
-        d1 = regs.R[b1]
-        regs.ZF = if d1 == d0: 1 else: 0
-        regs.CF = if d1 > d0: 1 else: 0
-      of CMPL:
-        cpu.read(b0)
-        if b0 > 8: exception
-        cpu.read(b1)
-        if b1 > 8: exception
-        dump opcode, b0.Regs, b1.Regs
-        b0  = LOW(regs.R[b0])
-        b1  = LOW(regs.R[b1])
-        regs.ZF = if b0 == b0: 1 else: 0
-        regs.CF = if b1 > b0: 1 else: 0
-      of PUSH:
-        cpu.read(b0)
-        if b0 > 8: exception
-        dump opcode, b0.Regs
-        dec(regs.SP)
-        if regs.SP == 0xffffffff.DWORD:
-          error "stack overflow"
-        stack[regs.SP] = regs.R[b0]
-      of POP:
-        cpu.read(b0)
-        dump opcode, b0.Regs
-        if b0 > 8: exception
-        if stack[regs.SP] == STACK_SIZE:
-          error "stack underflow"
-        regs.R[b0] = stack[regs.SP]
-        inc(regs.SP)
-      of PRNT:
-        dump opcode
-        if regs.SP == STACK_SIZE: exception
-        echo code[stack[regs.SP]]
-        inc(regs.SP)
-      of PRNTX:
-        dump opcode
-        if regs.SP == STACK_SIZE:
-          error "stack underflow"
-        echo "0x", code[stack[regs.SP]].toHex()
-        inc(regs.SP)
-      of PRNTS:
-        dump opcode
-        if regs.SP == STACK_SIZE:
-          error "stack underflow"
-        #cpu.read(b0, stack[regs.SP])
-        echo cast[cstring](addr code[stack[regs.SP]])
-        inc(regs.SP)
+  while true:
+    if cpu.regs.PC >= cpu.codeLen: exception
+    cpu.read(op)
+    case op
+    of NOP:
+      dump op
+      discard
+    of HALT:
+      dump op
+      break
+    of MOV:
+      cpu.read(b0)
+      cpu.read(b1)
+      dump op, b0.Regs, b1.Regs
+      if (b0 >= 0 and b0 <= 7) and (b1 >= 0 and b1 <= 7):
+        cpu.regs.R[b0] = cpu.regs.R[b1]
+      else:
+        error "invalid register"
+    of MOVMB:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(w0)
+      if w0 >= cpu.codeLen: exception
+      dump op, b0.Regs, w0
+      cpu.read(w1, w0)
+      cpu.regs.R[b0] = w1
+    of MOVMW:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(w0)
+      if w0 >= cpu.codeLen: exception
+      dump op, b0.Regs, w0
+      cpu.read(w1, w0)
+      cpu.regs.R[b0] = w1
+    of MOVB:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      dump op, b0.Regs, b1
+      cpu.regs.R[b0] = b1
+    of MOVW:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(w0)
+      dump MOVW, b0.Regs, w0
+      cpu.regs.R[b0] = w0
+    of MOVBM:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(w0)
+      dump op, b0.Regs, w0
+      if w0 >= cpu.codeLen:
+        exception
+      cpu.write(cpu.regs.R[b0].BYTE, w0)
+    of MOVWM:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(w0)
+      dump op, b0.Regs, w0
+      if w0 >= cpu.codeLen: exception
+      cpu.write(cpu.regs.R[b0], w0)
+    of MOVMRB:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      dump op, b0.Regs, b1.Regs
+      if b1 > 8: exception
+      if cpu.regs.R[b1] >= cpu.codeLen: exception
+      cpu.read(b2, cpu.regs.R[b1])
+      cpu.regs.R[b0] = b2
+    of MOVMRW:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      dump op, b0.Regs, b1.Regs
+      if b1 > 8: exception
+      if cpu.regs.R[b1] >= cpu.codeLen: exception
+      cpu.read(w0, cpu.regs.R[b1])
+      cpu.regs.R[b0] = w0
+    of MOVMD:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(d0)
+      dump op, b0.Regs, d0
+      if d0 >= cpu.codeLen: exception
+      cpu.regs.R[b0] = d0
+    of MOVD:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(d0)
+      dump op, b0.Regs, d0
+      if w0 >= cpu.codeLen: exception
+      cpu.regs.R[b0] = d0
+    of MOVDM:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(w0)
+      dump op, b0.Regs, w0
+      if w0 >= cpu.codeLen: exception
+      cpu.write(cpu.regs.R[b0], w0)
+    of MOVMRD:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      if cpu.regs.R[b1] >= cpu.codeLen: exception
+      cpu.read(d0, cpu.regs.R[b1])
+      cpu.regs.R[b0] = d0
+    of JMP:
+      cpu.read(w0)
+      dump op, w0
+      if w0 > cpu.codeLen: exception
+      cpu.regs.PC = w0
+    of JZ:
+      cpu.read(w0)
+      dump op, w0
+      if w0 > cpu.codeLen: exception
+      if cpu.regs.ZF == 1:
+        cpu.regs.PC = w0
+    of JNZ:
+      cpu.read(w0)
+      dump op, w0
+      if w0 > cpu.codeLen: exception
+      if cpu.regs.ZF == 1:
+        cpu.regs.PC = w0
+    of JAE:
+      cpu.read(w0)
+      dump op, w0
+      if w0 > cpu.codeLen: exception
+      if cpu.regs.ZF == 1 or cpu.regs.CF == 0:
+        cpu.regs.PC = w0
+    of JBE:
+      cpu.read(w0)
+      dump op, w0
+      if w0 > cpu.codeLen: exception
+      if cpu.regs.ZF == 1 or cpu.regs.CF == 1:
+        cpu.regs.PC = w0
+    of JB:
+      cpu.read(w0)
+      dump op, w0
+      if w0 > cpu.codeLen: exception
+      if cpu.regs.ZF == 0 and cpu.regs.CF == 1:
+        cpu.regs.PC = w0
+    of JA:
+      cpu.read(w0)
+      dump op, w0
+      if w0 > cpu.codeLen: exception
+      if cpu.regs.ZF == 0 and cpu.regs.CF == 0:
+        cpu.regs.PC = w0
+    of ADVR:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(w0)
+      dump op, b0.Regs, w0
+      w1 = cpu.regs.R[b0].WORD + w0
+      cpu.regs.ZF = if w1 == 0: 1 else: 0
+      cpu.regs.CF = if w1 < cpu.regs.R[b0]: 1 else: 0
+      cpu.regs.R[b0] = w1
+    of ADRR:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      d0 = cpu.regs.R[b0]
+      d1 = cpu.regs.R[b1]
+      d1 += d0
+      cpu.regs.R[b0] = d1
+      cpu.regs.ZF = if d1 == 0: 1 else: 0
+      cpu.regs.CF = if d1 < d0: 1 else: 0
+    of ADRRL:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      b2 = cpu.regs.R[b0].BYTE
+      b1 = cpu.regs.R[b1].BYTE
+      b1 = b1 + b2
+      cpu.regs.R[b0] = b1
+      cpu.regs.ZF = if b1 == 0: 1 else: 0
+      cpu.regs.CF = if b1 < b2: 1 else: 0
+    of SUBVR:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(w0)
+      dump op, b0.Regs, w0
+      w1 = cpu.regs.R[b0].WORD - w0
+      cpu.regs.ZF = if w1 == 0: 1 else: 0
+      cpu.regs.CF = if w1 > cpu.regs.R[b0]: 1 else: 0
+      cpu.regs.R[b0] = w1
+    of SUBRR:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      d0 = cpu.regs.R[b0]
+      d1 = cpu.regs.R[b1]
+      d1 -= d0
+      cpu.regs.R[b0] = d1
+      cpu.regs.ZF = if d1 == 0: 1 else: 0
+      cpu.regs.CF = if d1 > d0: 1 else: 0
+    of SUBRRL:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      b2 = cpu.regs.R[b0].BYTE
+      b1 = cpu.regs.R[b1].BYTE
+      b1 = b1 - b2
+      cpu.regs.R[b0] = b1
+      cpu.regs.ZF = if b1 == 0: 1 else: 0
+      cpu.regs.CF = if d1 > b2: 1 else: 0
+    of XOR:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      d0 = cpu.regs.R[b0]
+      d1 = cpu.regs.R[b1]
+      d0 = d0 xor d1
+      cpu.regs.ZF = if d0 == 0: 1 else: 0
+      cpu.regs.CF = 0
+      cpu.regs.R[b0] = d0
+    of XORL:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      b2 = cpu.regs.R[b0].BYTE
+      b1 = cpu.regs.R[b1].BYTE
+      b2 = b2 xor b1
+      cpu.regs.ZF = if b2 == 0: 1 else: 0
+      cpu.regs.CF = 0
+      cpu.regs.R[b0] = b2
+    of NOT:
+      cpu.read(b0)
+      if b0 > 8: exception
+      dump op, b0.Regs
+      cpu.regs.R[b0] = not cpu.regs.R[b0]
+    of NOTL:
+      cpu.read(b0)
+      if b0 > 8: exception
+      #cast[ptr BYTE](addr cpu.regs.R[b0])[] = not cpu.regs.R[b0]
+    of ADVRD:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(d0)
+      dump op, b0.Regs, d0
+      d1 = cpu.regs.R[b0] + d0
+      cpu.regs.ZF = if d1 == 0: 1 else: 0
+      cpu.regs.CF = if d1 < cpu.regs.R[b0]: 1 else: 0
+      cpu.regs.R[b0] = d1
+    of SUBVRD:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(d0)
+      dump op, b0.Regs, d0
+      d1 = cpu.regs.R[b0] - d0
+      cpu.regs.ZF = if d1 == 0: 1 else: 0
+      cpu.regs.CF = if d1 > cpu.regs.R[b0]: 1 else: 0
+      cpu.regs.R[b0] = d1
+    of CMP:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      d0 = cpu.regs.R[b0]
+      d1 = cpu.regs.R[b1]
+      cpu.regs.ZF = if d1 == d0: 1 else: 0
+      cpu.regs.CF = if d1 > d0: 1 else: 0
+    of CMPL:
+      cpu.read(b0)
+      if b0 > 8: exception
+      cpu.read(b1)
+      if b1 > 8: exception
+      dump op, b0.Regs, b1.Regs
+      b0  = LOW(cpu.regs.R[b0])
+      b1  = LOW(cpu.regs.R[b1])
+      cpu.regs.ZF = if b0 == b0: 1 else: 0
+      cpu.regs.CF = if b1 > b0: 1 else: 0
+    of PUSH:
+      cpu.read(b0)
+      if b0 > 8: exception
+      dump op, b0.Regs
+      dec(cpu.regs.SP)
+      if cpu.regs.SP == 0xffffffff.DWORD:
+        error "stack overflow"
+      cpu.stack[cpu.regs.SP] = cpu.regs.R[b0]
+    of POP:
+      cpu.read(b0)
+      dump op, b0.Regs
+      if b0 > 8: exception
+      if cpu.stack[cpu.regs.SP] == STACK_SIZE:
+        error "stack underflow"
+      cpu.regs.R[b0] = cpu.stack[cpu.regs.SP]
+      inc(cpu.regs.SP)
+    of PRNT:
+      dump op
+      if cpu.regs.SP == STACK_SIZE: exception
+      echo cpu.code[cpu.stack[cpu.regs.SP]]
+      inc(cpu.regs.SP)
+    of PRNTX:
+      dump op
+      if cpu.regs.SP == STACK_SIZE:
+        error "stack underflow"
+      echo "0x", cpu.code[cpu.stack[cpu.regs.SP]].toHex()
+      inc(cpu.regs.SP)
+    of PRNTS:
+      dump op
+      if cpu.regs.SP == STACK_SIZE:
+        error "stack underflow"
+      echo cast[cstring](addr cpu.code[cpu.stack[cpu.regs.SP]])
+      inc(cpu.regs.SP)
---
+++ vcpu.nimble
@@ -0,0 +1,8 @@
+version       = "1.0.1"
+author        = "Huy Doan"
+description   = "Virtual CPU for code obfuscation"
+license       = "MIT"
+skipDirs      = @["examples", "tests"]
+srcDir        = "src"
+
+requires        "nim >= 1.2.0"
